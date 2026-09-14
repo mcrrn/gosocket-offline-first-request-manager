cb7425d194d26d6b1ba120eaa0101a148ecda709
martin <martincarreno@ug.uchile.cl>
2026-09-14 16:24:06 -0300

    fix(application): se corrige mensaje de conflicto

--- backend/src/OfflineRequests.Application/RequestConflictException.cs
+++ backend/src/OfflineRequests.Application/RequestConflictException.cs
@@ -3,7 +3,7 @@ namespace OfflineRequests.Application;
 public class RequestConflictException : Exception
 {
     public RequestConflictException(Guid id)
-        : base($"Una solicitud con el mismo ID ({id}) ya existe pero con contenido diferente.")
+        : base($"Una solicitud con el mismo ID ({id}) ya existe, pero con contenido diferente.")
     {
     }
 }
